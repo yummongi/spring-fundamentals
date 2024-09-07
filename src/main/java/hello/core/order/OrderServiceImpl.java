800bcc31bea9f7946b32e5ae22eac06be7e03125
yummongi <yummongi@gmail.com>
2024-09-07 15:23:59 +0900

    [섹션6] 싱글톤 컨테이너 학습 완료
    - 웹 애플리케이션과 싱글톤 패턴의 관계 이해
    - 스프링 컨테이너를 통한 싱글톤 패턴 구현 방식 학습
    - 싱글톤 방식의 주의점 파악
    - @Configuration과 싱글톤의 관계 학습
    - 바이트코드 조작을 통한 싱글톤 보장 방식 이해
    - README.md 업데이트: 학습 진행 상황 반영

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -16,11 +16,15 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
-
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
