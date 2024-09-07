52cbbb2032007eb1521f99dde69850147a539b8a
yummongi <yummongi@gmail.com>
2024-09-07 22:25:29 +0900

    [섹션8] 의존관계 자동 주입: 다양한 주입 방법과 롬복 학습
    - 다양한 의존관계 주입 방법 학습 (생성자, 수정자, 필드 주입)
    - 옵션 처리 방법 이해
    - 생성자 주입의 장점과 권장 이유 학습
    - 롬복 라이브러리 도입 및 최신 트렌드 적용
    - README.md 업데이트: 학습 진행 상황 반영

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,21 +4,40 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+/*
+    // 1. Spring Container에 Bean 등록 후
+    // 2. 의존 관계 주입 과정에서 주입
     @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+        @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+*/
+    /***
+     * lombok 적용으로 생성자 생략 가능
+     */
+/*    // 1. Spring Container에 Bean 등록과 함께 의존 관계 주입
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
+    // Spring Container에 Bean 등록과 동시에 의존 관계 주입
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
 public class OrderServiceTest {
